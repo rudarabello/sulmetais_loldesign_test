2eaa0905fbc604a40c74226026fafffc1130f2f9
rudarabello <ruda.rabello.silva@gmail.com>
2022-10-27 16:47:08 -0300

    feat: add tests to products auth

--- cypress/e2e/tests/login_products.cy.js
+++ cypress/e2e/tests/login_products.cy.js
@@ -1,11 +1,17 @@
-describe(' Make login with known data ', { execTimeout: 90000 }, () => {
+import { faker } from '@faker-js/faker';
+
+describe(' Make login with known data ', () => {
     const login = {
         name: 'Walmir',
         email: 'walmir.lucena@loldesign.com.br',
         senha: 'walmirAdmin123',
     };
+    const product = {
+        category: faker.name.firstName(),
+        name: faker.name.firstName(),
+    };
 
-    it('Should be make login with data sent by Floriza', () => {
+    it('Should make login with data sent by Floriza', () => {
         cy.visit('https://sulmetais-web-prod.loldesign.com.br/');
         cy.get("input[type='email']").type(login.email);
         cy.get("input[type='password']").type(login.senha);
@@ -15,11 +21,65 @@ describe(' Make login with known data ', { execTimeout: 90000 }, () => {
         cy.contains('Olá');
     });
 
-    it('Should be enter on products', () => {
+    it('Should enter on products and generate pdf', () => {
+        cy.visit('https://sulmetais-web-prod.loldesign.com.br/');
+        cy.get("input[type='email']").type(login.email);
+        cy.get("input[type='password']").type(login.senha);
+        cy.intercept('POST', 'https://sulmetais-api-prod.loldesign.com.br/api/login').as('newLogin');
+        cy.contains('Entrar').click();
+        cy.wait('@newLogin', { timeout: 15000 });
+        cy.contains('Olá');
         cy.intercept('GET', '	https://sulmetais-web-prod.loldesign.com.br/_next/static/chunks/pages/admin/products.js').as('Products');
         cy.contains('Produtos').click();
         cy.wait('@Products', { timeout: 15000 });
-        cy.get('.g-sidenav-show g-sidenav-pinned color').get('.data-nextjs-dialog-overlay').get('.button').click();
-        cy.contains('unhandled error', { timeout: 15000 });
+        cy.get('tbody').within(() => {
+            cy.get('tr').first().click();
+        });
+        cy.wait(5000);
+        cy.get('.modal-footer').within(() => {
+            cy.get('button').first().click();
+        });
+    });
+
+    it('Should add a fake category', () => {
+        cy.visit('https://sulmetais-web-prod.loldesign.com.br/');
+        cy.get("input[type='email']").type(login.email);
+        cy.get("input[type='password']").type(login.senha);
+        cy.intercept('POST', 'https://sulmetais-api-prod.loldesign.com.br/api/login').as('newLogin');
+        cy.contains('Entrar').click();
+        cy.wait('@newLogin', { timeout: 15000 });
+        cy.contains('Olá');
+        cy.intercept('GET', '	https://sulmetais-web-prod.loldesign.com.br/_next/static/chunks/pages/admin/products.js').as('Products');
+        cy.contains('Produtos').click();
+        cy.wait('@Products', { timeout: 15000 });
+        cy.wait(5000);
+        cy.get('button').should('contain', 'Adicionar').last().click();
+        cy.get('.modal-body').within(() => {
+            cy.get("input[type='text']").type(product.category);
+        });
+        cy.get("button[type='submit']").first().click();
+    });
+
+    it('Should add a fake product', () => {
+        cy.visit('https://sulmetais-web-prod.loldesign.com.br/');
+        cy.get("input[type='email']").type(login.email);
+        cy.get("input[type='password']").type(login.senha);
+        cy.intercept('POST', 'https://sulmetais-api-prod.loldesign.com.br/api/login').as('newLogin');
+        cy.contains('Entrar').click();
+        cy.wait('@newLogin', { timeout: 15000 });
+        cy.contains('Olá');
+        cy.intercept('GET', '	https://sulmetais-web-prod.loldesign.com.br/_next/static/chunks/pages/admin/products.js').as('Products');
+        cy.contains('Produtos').click();
+        cy.wait('@Products', { timeout: 15000 });
+        cy.wait(5000);
+        cy.get('button').should('contain', 'Adicionar').first().click();
+        cy.get('.modal-body').within(() => {
+            cy.get("input[type='text']").type(product.name);
+        });
+        cy.get('.modal-body').within(() => {
+            cy.get("button[type='button']").first().click();
+        });
+        //Due to the category combox not working
+        //it was not possible to do the full test in this modal
     });
 });
